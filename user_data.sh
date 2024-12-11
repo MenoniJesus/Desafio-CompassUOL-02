32d2a29cc4447a1c4dc76b1458fb7d918fca8e80
Leandro Menoni <144243042+MenoniJesus@users.noreply.github.com>
2024-12-11 16:39:25 -0300

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 sudo su
+
 yum update -y
 yum upgrade -y
 yum install -y docker
 systemctl start docker
 systemctl enable docker
+
+yum install mariadb -y
+
+mysql -h dbdesafio02.cby088cym3id.us-east-1.rds.amazonaws.com -P 3306 -u admin -ppKYuW9kbnWW04RDZZ3zp <<EOF
+create database dbDesafio02;
+exit;
+EOF
 
 mkdir -p /usr/lib/docker/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v2.30.3/docker-compose-linux-x86_64 -o /usr/lib/docker/cli-plugins/docker-compose
@@ -24,10 +32,10 @@ services:
     ports:
       - 8080:80
     environment:
-      WORDPRESS_DB_HOST: <RDS_ENDPOINT>
-      WORDPRESS_DB_USER: wordpress
-      WORDPRESS_DB_PASSWORD: wordpress
-      WORDPRESS_DB_NAME: wordpress
+      WORDPRESS_DB_HOST: dbdesafio02.cby088cym3id.us-east-1.rds.amazonaws.com
+      WORDPRESS_DB_USER: admin
+      WORDPRESS_DB_PASSWORD: pKYuW9kbnWW04RDZZ3zp
+      WORDPRESS_DB_NAME: dbDesafio02
     volumes:
       - wp-data:/var/www/html
 
@@ -36,5 +44,4 @@ volumes:
 EOL
 
 chown -R ec2-user:ec2-user /home/ec2-user/wordpress
-
 docker compose -f /home/ec2-user/wordpress/docker-compose.yml up -d
